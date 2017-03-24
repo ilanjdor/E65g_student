1bde980291210b0a28fa116cbe1ab113bf49ba16
Ilan Dor <ilanjdor@gmail.com>
2017-03-24 00:23:12 -0500

    A3 Q6 complete

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myGrid: GridView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func advanceGrid(_ sender: Any) {
+        myGrid?.grid = (myGrid?.grid.next())!
+        if (myGrid?.grid) != nil {
+            myGrid.setNeedsDisplay()
+        }
+    }
 }
 
